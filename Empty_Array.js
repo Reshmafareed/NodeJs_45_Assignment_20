970f4d224bca899efd317ba94d273f44eabe74bc
Reshmafareed <163650782+Reshmafareed@users.noreply.github.com>
2024-09-11 12:15:58 +0500

    Add files via upload

---
+++ Empty_Array.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Think of something you could store in a array.
+let items = ['pakistan', 'karachi', 'Urdu'];
+// Write a program that creates a list containing these items.
+let item2 = [];
+item2.push('Germany');
+item2.push("England");
+item2.push('India');
+console.log(item2);
---
+++ Empty_Array.ts
@@ -0,0 +1,14 @@
+import { it } from "node:test";
+
+//Think of something you could store in a array.
+let items:string[] = ['pakistan','karachi','Urdu']
+
+// Write a program that creates a list containing these items.
+
+let item2 :string[] = [];
+
+item2.push('Germany');
+item2.push("England");
+item2.push('India');
+
+console.log(item2);
